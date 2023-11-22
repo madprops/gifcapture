ae440999ff7c2c4a84f6c899b6f1bc55d80dd89b
Auric Vente <mprops@protonmail.com>
2023-11-22 07:31:26 -0600

    Mods

--- gifcapture.rb
+++ gifcapture.rb
@@ -1,9 +1,15 @@
 #!/usr/bin/env ruby
 unixtime = Time.now.to_i
-rootdir = "/home/yo/Downloads/pics/caps/"
+rootdir = "/home/yo/Downloads/pics/caps"
 dirname = "#{rootdir}/#{unixtime}"
+
+unless Dir.exist?(dirname)
+  Dir.mkdir(dirname)
+end
+
 points = `xrectsel`
 names = []
+sleep(0.1)
 
 for i in 1..5 do
   name = "#{dirname}/#{i}.jpg"
@@ -14,4 +20,4 @@ end
 
 flat = names.join(" ")
 `convert -delay 33 -loop 0 #{flat} -quality 75 #{dirname}/result.gif`
-`notify-send "GIF created"`
+`notify-send "GIF created in #{dirname}"`
