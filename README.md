d22db74f1b512405b491764aad820ff8d3b00477
Auric Vente <mprops@protonmail.com>
2023-11-25 00:12:43 -0600

    Mods

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-You will need `xrectsel`, `import`, `ffmpeg`, `notify-send`.
+You will need `xrectsel`, `scrot`, `ffmpeg`, `notify-send`.
 
 ![](https://i.imgur.com/LGil6hq.jpg)
--- gifcapture.rb
+++ gifcapture.rb
@@ -1,44 +1,64 @@
 #!/usr/bin/env ruby
-interval = 0.777
-framerate = 3.33
-num_images = 3
-image_ext = "png"
-rootdir = "/home/yo/Downloads/pics/caps"
-dirname = "#{rootdir}/#{Time.now.to_i}"
-$consonants = ("a".."z").to_a - ["a", "e", "i", "o", "u"]
-$vowels = ["a", "e", "i", "o", "u"]
+
+# CONFIG
+$interval = 0.777
+$delay = 33
+$quality = 80
+$num_images = 3
+$image_ext = "png"
+$rootdir = "/home/yo/Downloads/pics/caps"
+$dirname = "#{$rootdir}/#{Time.now.to_i}"
+# END CONFIG
+
+def mkdir()
+  unless Dir.exist?($dirname)
+    Dir.mkdir($dirname)
+  end
+end
+
+def capture()
+  points = `xrectsel`
+  sleep(0.1)
+  split = points.split("+")
+  geometry = split[0].split("x")
+  width = geometry[0]
+  height = geometry[1]
+  x = split[1]
+  y = split[2]
+
+  for i in 1..$num_images
+    name = "#{$dirname}/#{i}.#{$image_ext}"
+    `import -window root -crop "#{points}" #{name}`
+    `scrot -o #{name} -a #{width},#{height},#{x},#{y}`
+
+    if i == $num_images
+      break
+    end
+
+    sleep($interval)
+  end
+end
 
 def tagname(num)
+  consonants = ("a".."z").to_a - ["a", "e", "i", "o", "u"]
+  vowels = ["a", "e", "i", "o", "u"]
   word = ""
 
   num.times do
-    word += "#{$consonants.sample}#{$vowels.sample}"
+    word += "#{consonants.sample}#{vowels.sample}"
   end
 
   return word
 end
 
-unless Dir.exist?(dirname)
-  Dir.mkdir(dirname)
+def render()
+  files = "#{$dirname}/*.#{$image_ext}"
+  output = "#{$dirname}/#{tagname(3)}.gif"
+  `convert -delay #{$delay} -loop 0 #{files} -quality #{$quality} #{output}`
+  `notify-send "GIF saved as #{output}"`
 end
 
-points = `xrectsel`
-sleep(0.1)
-names = []
-
-for i in 1..num_images
-  name = "#{dirname}/#{i}.#{image_ext}"
-  names.push(name)
-  `import -window root -crop "#{points}" #{name}`
-
-  if i == num_images
-    break
-  end
-
-  sleep(interval)
-end
-
-files = "#{dirname}/%d.#{image_ext}"
-output = "#{dirname}/#{tagname(3)}.gif"
-`ffmpeg -framerate #{framerate} -i #{files} #{output} -y`
-`notify-send "GIF saved as #{output}"`
+# MAIN
+mkdir()
+capture()
+render()
