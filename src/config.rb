669b09fd4ff24460419f0d2c8092d246e02bab2d
Auric Vente <mprops@protonmail.com>
2023-11-25 21:34:16 -0600

    Mods

--- src/config.rb
+++ src/config.rb
@@ -1,6 +1,18 @@
+# Capture an image very x seconds
 $interval = 0.777
+
+# Delay between gif frames
+# The lower this is the faster the gif will be
 $delay = 33
+
+# Quality percentage of the gif
 $quality = 80
+
+# Number of images to capture
 $num_images = 5
+
+# Save images using this extension
 $image_ext = "png"
+
+# Root directory to save images
 $rootdir = "/home/yo/Downloads/pics/caps"
