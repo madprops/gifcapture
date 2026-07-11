b72c3ec9e097459069b2b3a1e20d4cb7488b9293
Auric Vente <easywest@protonmail.com>
2026-07-10 22:33:13 -0600

    Mods

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Ruby GIF screen capture script environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      forEachSystem = nixpkgs.lib.genAttrs systems;
+    in {
+      devShells = forEachSystem (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              ruby
+              xrectsel
+              scrot
+              imagemagick
+              libnotify
+              xdg-utils
+            ];
+          };
+        }
+      );
+    };
+}
